714542c2d982fa0ec5867560c47e980c1410dc0e
Siddharth Sindhi <siddharth.sindhi@evontech.com>
2019-07-29 15:06:23 +0530

    new comment added

--- MyGITDemoApp/ViewController.swift
+++ MyGITDemoApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("testing commit on GIT")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
