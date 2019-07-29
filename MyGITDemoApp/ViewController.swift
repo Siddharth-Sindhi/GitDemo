a74a54f7ecc8ef4838c418a6dcd3780764b32b56
Siddharth Sindhi <siddharth.sindhi@evontech.com>
2019-07-29 15:11:54 +0530

    test

--- MyGITDemoApp/ViewController.swift
+++ MyGITDemoApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("testing commit on GIT")
         print("new comment")
+        print("new comment in development")
+        print("new comment in development")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
